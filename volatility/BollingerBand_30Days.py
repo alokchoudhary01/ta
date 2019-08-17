f80c28328a6c45c984bf7020f30e78d399b63f1b
alokchoudhary01 <54199734+alokchoudhary01@users.noreply.github.com>
2019-08-17 21:24:10 +0530

    Add files via upload

---
+++ volatility/BollingerBand_30Days.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from pandas_datareader import data as web
+
+# Make function for calls to Yahoo Finance
+def get_adj_close(ticker, start, end):
+    '''
+    A function that takes ticker symbols, starting period, ending period
+    as arguments and returns with a Pandas DataFrame of the Adjusted Close Prices
+    for the tickers from Yahoo Finance
+    '''
+    start = start
+    end = end
+    info = web.DataReader(ticker, data_source='yahoo', start=start, end=end)['Adj Close']
+    return pd.DataFrame(info)
+
+# Get Adjusted Closing Prices for Facebook, Tesla and Amazon between 2016-2017
+fb = get_adj_close('fb', '1/2/2016', '31/12/2017')
+tesla = get_adj_close('tsla', '1/2/2016', '31/12/2017')
+amazon = get_adj_close('amzn', '1/2/2016', '31/12/2017')
+
+# Calculate 30 Day Moving Average, Std Deviation, Upper Band and Lower Band
+for item in (fb, tesla, amazon):
+    item['30 Day MA'] = item['Adj Close'].rolling(window=20).mean()
+    item['30 Day STD'] = item['Adj Close'].rolling(window=20).std()
+    item['Upper Band'] = item['30 Day MA'] + (item['30 Day STD'] * 2)
+    item['Lower Band'] = item['30 Day MA'] - (item['30 Day STD'] * 2)
+
+# Simple 30 Day Bollinger Band for Facebook (2016-2017)
+fb[['Adj Close', '30 Day MA', 'Upper Band', 'Lower Band']].plot(figsize=(12,6))
+plt.title('30 Day Bollinger Band for Facebook')
+plt.ylabel('Price (USD)')
+plt.show();
